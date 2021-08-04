98c5190d4dcd5c80dbd3baa1b82d915e44e6d5a2
iproman <itechman@yandex.ru>
2021-08-04 09:11:00 +0500

    Add icons in menu

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -9,7 +9,14 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  labels = [
+    {title: 'Search', url: '/folder/Inbox', icon: 'search'},
+    {title: 'Reload', url: '/folder/Favorites', icon: 'reload'},
+    {title: 'Submenu', url: '/folder/Favorites', icon: 'ellipsis-vertical'},
+  ];
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
