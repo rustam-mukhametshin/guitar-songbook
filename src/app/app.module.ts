0eb471db235ae0c88884ae2518f48a131c96cb7b
iproman <itechman@yandex.ru>
2021-08-04 09:50:59 +0500

    Generate nav c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SidebarComponent } from './views/layout/sidebar/sidebar.component';
+import { NavComponent } from './views/layout/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
+    NavComponent,
   ],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
