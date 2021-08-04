748af9f7d5baff55af6e0995079b8f5faa4331e6
iproman <itechman@yandex.ru>
2021-08-04 07:28:29 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'guitar-songbook',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
