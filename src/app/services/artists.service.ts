cb60716b8df236469befc2f0bcdb3074b0a7ea47
iproman <itechman@yandex.ru>
2021-08-06 16:05:34 +0500

    Add artists service

---
+++ src/app/services/artists.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Artist } from '../interfaces/Artist';
+import { Song } from '../interfaces/Song';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArtistsService {
+
+  private readonly apiUrl: string = environment.apiUrl;
+
+  constructor(
+    private readonly httpClient: HttpClient
+  ) {
+  }
+
+  getArtists(): Observable<Artist[]> {
+    return this.httpClient.get<Artist[]>(this.apiUrl + '/users/');
+  }
+
+  getArtist(id: number | string): Observable<Artist> {
+    return this.httpClient.get<Artist>(this.apiUrl + '/posts/' + id);
+  }
+
+  getPostsByUserId(userId: number | string): Observable<Song[]> {
+    return this.httpClient.get<Song[]>(this.apiUrl + '/posts?userId=' + userId);
+  }
+
+  getSong(id: number | string): Observable<Song> {
+    return this.httpClient.get<Song>(this.apiUrl + '/posts/' + id);
+  }
+}
