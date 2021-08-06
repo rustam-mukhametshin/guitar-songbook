ccde381ed8eae23f8010fa6fb0dee4d4c7cd9c28
iproman <itechman@yandex.ru>
2021-08-06 15:58:38 +0500

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,31 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SidebarComponent } from './views/layout/sidebar/sidebar.component';
 import { NavComponent } from './views/layout/nav/nav.component';
+import { ArtistsComponent } from './views/artists/artists.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SongsComponent } from './views/songs/songs.component';
+import { SongComponent } from './views/song/song.component';
+import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     NavComponent,
+    ArtistsComponent,
+    SongsComponent,
+    SongComponent,
+    NotFoundPageComponent,
   ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule
+  ],
   providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
