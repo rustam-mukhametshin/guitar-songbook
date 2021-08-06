bd12086e27362b5cbe700ed066ddb03d7460deff
iproman <itechman@yandex.ru>
2021-08-06 15:59:35 +0500

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { ArtistsComponent } from './views/artists/artists.component';
+import { SongsComponent } from './views/songs/songs.component';
+import { SongComponent } from './views/song/song.component';
+import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   {
+    path: 'artists',
+    component: ArtistsComponent,
+  },
+  {
+    path: 'songs/:artistId',
+    component: SongsComponent,
+  },
+  {
+    path: 'song/:id',
+    component: SongComponent,
+  },
+  {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'artists',
     pathMatch: 'full'
   },
   {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+  {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
   }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
