4c63027b4a987a40459963cc45eaf519dce9c739
iproman <itechman@yandex.ru>
2021-08-11 16:46:59 +0500

    Add components

--- src/app/views/songs/songs.module.ts
+++ src/app/views/songs/songs.module.ts
@@ -12,9 +12,15 @@ import { CommonModule } from '@angular/common';
 import { SongsRoutingModule } from './songs-routing.module';
 import { SharedModule } from '../../modules/shared.module';
 import { SongsComponent } from './songs/songs.component';
+import { SongCreateComponent } from './song-create/song-create.component';
+import { SongUpdateComponent } from './song-update/song-update.component';
+import { SongFormComponent } from './song-form/song-form.component';
 
 const components = [
   SongsComponent,
+  SongCreateComponent,
+  SongUpdateComponent,
+  SongFormComponent,
 ];
 
 @NgModule({
