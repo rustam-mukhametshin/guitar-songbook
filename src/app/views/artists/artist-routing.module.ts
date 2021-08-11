fbc2aa757fe3275e8c5003bf84d3fdad59c2eddf
iproman <itechman@yandex.ru>
2021-08-11 17:13:22 +0500

    Generate artist m

---
+++ src/app/views/artists/artist-routing.module.ts
@@ -0,0 +1,19 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ArtistRoutingModule {
+}
---
+++ src/app/views/artists/artist.module.ts
@@ -0,0 +1,23 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ArtistRoutingModule } from './artist-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ArtistRoutingModule
+  ]
+})
+export class ArtistModule {
+}
