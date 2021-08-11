013c13b283f103bc50712b694a20f1abce9d66d7
iproman <itechman@yandex.ru>
2021-08-11 15:54:08 +0500

    Generate shared module

---
+++ src/app/modules/shared.module.ts
@@ -0,0 +1,22 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CommonModule
+  ]
+})
+export class SharedModule {
+}
