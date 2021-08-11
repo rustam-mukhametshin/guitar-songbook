629510b7f16355e17718807e1bf7d72b32a079b1
iproman <itechman@yandex.ru>
2021-08-11 15:38:47 +0500

    Generate songs module/routing

---
+++ src/app/views/songs/songs.module.ts
@@ -0,0 +1,23 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SongsRoutingModule } from './songs-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SongsRoutingModule
+  ]
+})
+export class SongsModule {
+}
