4b4d657fa51d85a6a166df267c99e5961dce947c
iproman <itechman@yandex.ru>
2021-08-13 16:38:16 +0500

    Show list of custom songs

--- src/app/views/songs/songs/songs.component.ts
+++ src/app/views/songs/songs/songs.component.ts
@@ -12,6 +12,8 @@ import { ArtistsService } from '../../../services/artists.service';
 import { ActivatedRoute } from '@angular/router';
 import { Song } from '../../../interfaces/Song';
 import { Artist } from '../../../interfaces/Artist';
+import { SongService } from '../../../services/song.service';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -28,16 +30,25 @@ export class SongsComponent implements OnInit {
   showSearch = false;
   searchText: string;
 
+  private readonly type: string;
+  private readonly artistId: string;
+
   constructor(
     private readonly activatedRoute: ActivatedRoute,
-    private readonly artistsService: ArtistsService
+    private readonly artistsService: ArtistsService,
+    private readonly songService: SongService
   ) {
+    this.artistId = this.activatedRoute.snapshot.paramMap.get('artistId');
+    this.type = this.activatedRoute.snapshot.queryParamMap.get('type');
   }
 
   ngOnInit() {
-    const artistId = this.activatedRoute.snapshot.paramMap.get('artistId');
-    this.songs$ = this.artistsService.getPostsByUserId(artistId);
-    this.artist$ = this.artistsService.getArtist(artistId);
+
+    // Get list
+    this.initList();
+
+    // Get artist
+    this.artist$ = this.artistsService.getArtist(this.artistId);
   }
 
   addToFavourite(id: number | string) {
@@ -46,6 +57,34 @@ export class SongsComponent implements OnInit {
 
   removeFromFavourite(id: number | string) {
 
+  }
+
+  /**
+   * Get list of songs
+   * base on type
+   *
+   * @private
+   */
+  private initList() {
+    switch (this.type) {
+      case 'favourite':
+        // Todo
+        this.songs$ = this.artistsService.getPostsByUserId(this.artistId);
+        break;
+      case 'custom':
+        this.songService.loadSavedSongs();
+
+        this.songs$ = this.songService.songsCustom$.pipe(
+          map(songs => songs.filter(
+              song => song.userId === Number.parseInt(this.artistId, 10)
+            )
+          )
+        );
+        break;
+      default:
+        this.songs$ = this.artistsService.getPostsByUserId(this.artistId);
+        break;
+    }
   }
 
 }
