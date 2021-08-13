b4ccae2dbe4ee5a022013aa06b0f535ffd0c40a8
iproman <itechman@yandex.ru>
2021-08-13 13:58:14 +0500

    Add tests for storage

--- src/app/services/storages/storage.service.spec.ts
+++ src/app/services/storages/storage.service.spec.ts
@@ -10,15 +10,101 @@ import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
 
-xdescribe('StorageService', () => {
+describe('StorageService', () => {
   let service: StorageService;
+  const fixtureData = {
+    key: 'test',
+    value: 'someValue'
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StorageService);
   });
 
+  beforeEach(() => {
+    // Clear storage
+    service.clear();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#get should check existed value', done => {
+    service.get(fixtureData.key).then(val => {
+      expect(val.value).toBeNull();
+      done();
+    });
+  });
+
+  it('#set should check saving value', done => {
+    service.set(fixtureData.key, fixtureData.value).then(_ => {
+      service.get(fixtureData.key).then(res => {
+        expect(res.value).toEqual(fixtureData.value);
+        done();
+      });
+    });
+  });
+
+  it('#remove should check removing key with values', done => {
+    // Check for null
+    service.get(fixtureData.key).then(val => {
+      expect(val.value).toBeNull();
+      done();
+    });
+
+    // Set data
+    service.set(fixtureData.key, fixtureData.value).then(_ => {
+      done();
+    });
+
+    // Check for saving
+    service.get(fixtureData.key).then(res => {
+      expect(res.value).toEqual(fixtureData.value);
+      done();
+    });
+
+    // Remove data
+    service.remove(fixtureData.key).then(_ => {
+      done();
+    });
+
+    // Check for null
+    service.get(fixtureData.key).then(val => {
+      expect(val.value).toBeNull();
+      done();
+    });
+  });
+
+  it('#clear should check clearing storage', done => {
+    // Check for null
+    service.get(fixtureData.key).then(val => {
+      expect(val.value).toBeNull();
+      done();
+    });
+
+    // Set data
+    service.set(fixtureData.key, fixtureData.value).then(_ => {
+      done();
+    });
+
+    // Check for saving
+    service.get(fixtureData.key).then(res => {
+      expect(res.value).toEqual(fixtureData.value);
+      done();
+    });
+
+    // Clear data
+    service.clear().then(_ => {
+      done();
+    });
+
+    // Check for null
+    service.get(fixtureData.key).then(val => {
+      expect(val.value).toBeNull();
+      done();
+    });
+  });
+
 });
