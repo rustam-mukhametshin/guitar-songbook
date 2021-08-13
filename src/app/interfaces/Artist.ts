b4018360e624bc08aedf8741cb68ea38803672c2
iproman <itechman@yandex.ru>
2021-08-13 17:13:00 +0500

    Not used property

--- src/app/interfaces/Artist.ts
+++ src/app/interfaces/Artist.ts
@@ -1,5 +1,12 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
 export interface Artist {
   id: number;
   name: string;
-  username: boolean;
 }
