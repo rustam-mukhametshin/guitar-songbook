9c71c8cc5583e99645bc0c9021781f562223d714
iproman <itechman@yandex.ru>
2021-08-16 17:40:38 +0500

    Add test for #getArtists

--- src/app/services/artists.service.spec.ts
+++ src/app/services/artists.service.spec.ts
@@ -9,16 +9,74 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArtistsService } from './artists.service';
+import { ArtistStorageService } from './storages/artist-storage.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { first, of } from 'rxjs';
+import { Artist } from '../interfaces/Artist';
 
-xdescribe('ArtistsService', () => {
-  let service: ArtistsService;
+describe('ArtistsService', () => {
+  let artistsService: ArtistsService;
+  const artistsServiceSpy = jasmine.createSpyObj('ArtistsService', [
+    'getArtists'
+  ]);
+
+  let fake: Artist[];
+  let fake2: Artist[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ArtistsService);
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ArtistsService,
+          useValue: artistsServiceSpy
+        },
+        ArtistStorageService,
+        HttpClient
+      ],
+      imports: [
+        HttpClientModule
+      ]
+    });
+    artistsService = TestBed.inject(ArtistsService);
+  });
+
+  beforeEach(() => {
+    fake = [
+      {
+        id: 1,
+        name: 'test',
+      },
+      {
+        id: 2,
+        name: 'test2',
+      }
+    ] as Artist[];
+    fake2 = [
+      {
+        id: 1,
+        name: 'test3',
+      },
+      {
+        id: 2,
+        name: 'test2',
+      }
+    ] as Artist[];
+
+    artistsServiceSpy.getArtists.and.returnValue(of(fake));
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(artistsService).toBeTruthy();
+  });
+
+  it('#getArtists should return list of artists', done => {
+    artistsService.getArtists()
+      .pipe(
+        first()
+      )
+      .subscribe(c => {
+        expect(c).toEqual(fake);
+        done();
+      });
   });
 });
