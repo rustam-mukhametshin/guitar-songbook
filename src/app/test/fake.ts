b5fb5bfc58cb6b53e3b6a8a7ffb9329d291eb14b
iproman <itechman@yandex.ru>
2021-08-18 15:42:21 +0500

    Add fake obj for tests

---
+++ src/app/test/fake.ts
@@ -0,0 +1,43 @@
+/*
+ * Guitar songbook project
+ *
+ * @author Rustam Mukhametshin <https://github.com/iproman>
+ * @link https://github.com/iproman
+ * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
+ */
+
+import { Artist } from '../interfaces/Artist';
+
+export class Fake {
+  artists: Artist[] = [
+    {
+      id: 1,
+      name: 'test',
+    },
+    {
+      id: 2,
+      name: 'test2',
+    },
+    {
+      id: 3,
+      name: 'test3',
+    }
+  ];
+}
+
+export class Fake2 {
+  artists: Artist[] = [
+    {
+      id: 4,
+      name: 'test4',
+    },
+    {
+      id: 5,
+      name: 'test5',
+    },
+    {
+      id: 6,
+      name: 'test6',
+    }
+  ];
+}
