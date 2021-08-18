53d10c7f8d9c178fb2f138c2601cc89321d98c45
iproman <itechman@yandex.ru>
2021-08-18 15:46:49 +0500

    Add test for #get

--- src/app/services/storages/artist-storage.service.spec.ts
+++ src/app/services/storages/artist-storage.service.spec.ts
@@ -9,16 +9,49 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArtistStorageService } from './artist-storage.service';
+import { Artist } from '../../interfaces/Artist';
+import { of } from 'rxjs';
+import { Fake, Fake2 } from '../../test/fake';
+import SpyObj = jasmine.SpyObj;
 
-xdescribe('ArtistStorageService', () => {
+
+describe('ArtistStorageService', () => {
   let service: ArtistStorageService;
+  const artistStorageServiceSpy = jasmine.createSpyObj('ArtistStorageService', [
+    'get'
+  ]) as SpyObj<ArtistStorageService>;
+
+  let fake: Artist[];
+  let fake2: Artist[];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ArtistStorageService,
+          useValue: artistStorageServiceSpy
+        }
+      ]
+    });
     service = TestBed.inject(ArtistStorageService);
+  });
+
+  beforeEach(() => {
+    fake = Fake.artists;
+    fake2 = Fake2.artists;
+
+    artistStorageServiceSpy.get.and.returnValue(of(fake));
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#get should get all songs from storage', done => {
+    artistStorageServiceSpy.get()
+      .subscribe(v => {
+        expect(v).toEqual(fake);
+        done();
+      });
   });
 });
