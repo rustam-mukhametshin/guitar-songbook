c08895311be92c3160cb945a6818880e5364c8b1
iproman <itechman@yandex.ru>
2021-08-19 17:27:34 +0500

    Add songs fixture

--- src/app/test/fake.ts
+++ src/app/test/fake.ts
@@ -7,8 +7,12 @@
  */
 
 import { Artist } from '../interfaces/Artist';
+import { Song } from '../interfaces/Song';
 
 export class Fake {
+  /**
+   * Artists
+   */
   static artists: Artist[] = [
     {
       id: 1,
@@ -23,9 +27,50 @@ export class Fake {
       name: 'test3',
     }
   ];
+  /**
+   * Songs
+   */
+  static songs: Song[] = [
+    {
+      id: 1,
+      title: 'title 1',
+      body: '',
+      comments: 'Comments 1',
+      isCustom: false,
+      initialPause: 111,
+      duration: 111,
+      userId: 111,
+      isFavourite: false,
+    },
+    {
+      id: 2,
+      title: 'title 2',
+      body: '',
+      comments: 'Comments 2',
+      isCustom: false,
+      initialPause: 222,
+      duration: 222,
+      userId: 222,
+      isFavourite: false,
+    },
+    {
+      id: 3,
+      title: 'title 3',
+      body: '',
+      comments: 'Comments 3',
+      isCustom: false,
+      initialPause: 333,
+      duration: 333,
+      userId: 333,
+      isFavourite: false,
+    }
+  ];
 }
 
 export class Fake2 {
+  /**
+   * Artists
+   */
   static artists: Artist[] = [
     {
       id: 4,
@@ -38,6 +83,44 @@ export class Fake2 {
     {
       id: 6,
       name: 'test6',
+    }
+  ];
+  /**
+   * Songs
+   */
+  static songs: Song[] = [
+    {
+      id: 4,
+      title: 'title 4',
+      body: '',
+      comments: 'Comments 4',
+      isCustom: false,
+      initialPause: 444,
+      duration: 444,
+      userId: 444,
+      isFavourite: false,
+    },
+    {
+      id: 5,
+      title: 'title 5',
+      body: '',
+      comments: 'Comments 5',
+      isCustom: false,
+      initialPause: 555,
+      duration: 555,
+      userId: 555,
+      isFavourite: false,
+    },
+    {
+      id: 6,
+      title: 'title 6',
+      body: '',
+      comments: 'Comments 6',
+      isCustom: false,
+      initialPause: 666,
+      duration: 666,
+      userId: 666,
+      isFavourite: false,
     }
   ];
 }
