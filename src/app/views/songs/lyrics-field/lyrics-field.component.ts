2fd64aa7f4a1109628e2f6e5cd9df47f38ed422f
iproman <itechman@yandex.ru>
2021-08-23 12:25:39 +0500

    Add lyrics as controlValueAccessor

--- src/app/views/songs/lyrics-field/lyrics-field.component.ts
+++ src/app/views/songs/lyrics-field/lyrics-field.component.ts
@@ -6,19 +6,61 @@
  * @copyright Copyright (c) Rustam Mukhametshin, LLC, 2021
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
 
 @Component({
   selector: 'app-lyrics-field',
   templateUrl: './lyrics-field.component.html',
   styleUrls: ['./lyrics-field.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: LyricsFieldComponent,
+      multi: true
+    }
+  ]
 })
-export class LyricsFieldComponent implements OnInit {
+export class LyricsFieldComponent implements ControlValueAccessor {
+
+  @Output() isLyrics: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  onChange: (value: string) => void;
+  onTouched: () => void;
+
+  value: string;
+  disabled: boolean;
 
   constructor() {
   }
 
-  ngOnInit() {
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(obj: string): void {
+    this.value = obj;
+  }
+
+  set(val: string) {
+    if (this.disabled) {
+      return;
+    }
+
+    this.value = val;
+    this.onChange(this.value);
+    this.onTouched();
+
+    this.isLyrics.emit(false);
   }
 
 }
