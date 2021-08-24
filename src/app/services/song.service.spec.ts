bfb6ec5f0c25319baf09c0cd6564cd9f8e34aee2
iproman <itechman@yandex.ru>
2021-08-24 09:05:26 +0500

    Add test for #loadSavedSongs

--- src/app/services/song.service.spec.ts
+++ src/app/services/song.service.spec.ts
@@ -10,17 +10,28 @@ import { TestBed } from '@angular/core/testing';
 
 import { SongService } from './song.service';
 import { HttpClientModule } from '@angular/common/http';
+import { SongStorageService } from './storages/song-storage.service';
+import { first, of } from 'rxjs';
+import { Fake } from '../test/fake';
+import { take } from 'rxjs/operators';
 
 describe('SongService', () => {
   let service: SongService;
+  let storage: SongStorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
-      ]
+      ],
     });
     service = TestBed.inject(SongService);
+    storage = TestBed.inject(SongStorageService);
+  });
+
+  beforeEach(() => {
+    // Fake for storage
+    spyOn(storage, 'get').and.returnValue(of(Fake.songs));
   });
 
   it('should be created', () => {
@@ -31,8 +42,23 @@ describe('SongService', () => {
     done();
   });
 
-  xit('#loadSavedSongs should Load saved songs', done => {
-    done();
+  it('#loadSavedSongs should Load saved songs', done => {
+    service.songsCustom$
+      .pipe(take(1))
+      .subscribe(v => {
+        expect(v).toEqual([]);
+        done();
+      });
+
+    service.loadSavedSongs();
+
+    service.songsCustom$
+      .pipe(first())
+      .subscribe(v => {
+        expect(v).toEqual(Fake.songs);
+        done();
+      });
+
   });
 
   xit('#getSongsByArtistId should Get songs by artist id', done => {
